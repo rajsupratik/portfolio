84b8487df0e9e9f789959755e45cac88c1e919a6
rajsupratik <65940767+rajsupratik@users.noreply.github.com>
2024-09-26 13:23:05 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './Hero.css'
 import profile_img from '../../assets/profile_img.svg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import photo from '../../assets/photo_2024-09-10_22-40-29.jpg';
 
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-      <img src="photo_2024-09-10_22-40-29.jpg" alt="" className='profile-img' />
+      <img src={photo} alt="n" className='profile-img' />
       <h1><span>I'm Rajsu Pratik,</span> Python developer based in INDIA.</h1>
       <p>A passionate cybersecurity specialist and Python enthusiast. As a master's student deeply invested in cloud computing and advanced technologies.</p>
       <div className="hero-action">
